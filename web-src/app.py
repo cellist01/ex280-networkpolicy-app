b40389c7a134de2ba8dfc4eca5298b8ca75c3ad0
Cho Hang Woon <cellist01@gmail.com>
2024-06-24 15:13:56 +0900

    update source

--- web-src/app.py
+++ web-src/app.py
@@ -24,7 +24,7 @@ def check_db_connection():
     while True:
         try:
             with app.app_context():
-                db.session.execute('SELECT 1')
+                db.session.execute('SHOW DATABASE;')
                 print("Database connection is active.")
         except Exception as e:
             print(f"Database connection error: {str(e)}")
